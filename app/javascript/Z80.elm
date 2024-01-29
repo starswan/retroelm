b8059d73c5bc834ce641c35e66ef95925ecc00d6
starswan <github@starswan.com>
2024-01-29 22:11:16 +0000

    vector 90 - 97

--- app/javascript/Z80.elm
+++ app/javascript/Z80.elm
@@ -785,7 +785,17 @@ lt40_dict_lite = Dict.fromList
           -- case 0x8B: adc(E); break;
           (0x8B, (\z80 -> z80 |> set_flag_regs (adc z80.main.e z80.flags))),
           -- case 0x8F: adc(A); break;
-          (0x8F, (\z80 -> z80 |> set_flag_regs (z80_sub z80.flags.a z80.flags)))
+          (0x8F, (\z80 -> z80 |> set_flag_regs (z80_sub z80.flags.a z80.flags))),
+          -- case 0x90: sub(B); break;
+          (0x90, (\z80 -> z80 |> set_flag_regs (z80_sub z80.main.b z80.flags))),
+          -- case 0x91: sub(C); break;
+          (0x91, (\z80 -> z80 |> set_flag_regs (z80_sub z80.main.c z80.flags))),
+          -- case 0x92: sub(D); break;
+          (0x92, (\z80 -> z80 |> set_flag_regs (z80_sub z80.main.d z80.flags))),
+           -- case 0x93: sub(E); break;
+          (0x93, (\z80 -> z80 |> set_flag_regs (z80_sub z80.main.e z80.flags))),
+          -- case 0x97: sub(A); break;
+          (0x97, (\z80 -> z80 |> set_flag_regs (z80_sub z80.flags.a z80.flags)))
     ]
 
 lt40_dict: Dict Int (IXIYHL -> Z80 -> Z80)
@@ -979,7 +989,19 @@ lt40_dict = Dict.fromList
           (0x8E, (\ixiyhl z80 -> let
                                     value = hl_deref_with_z80 ixiyhl z80
                                  in
-                                    value.z80 |> set_flag_regs (adc value.value z80.flags)))
+                                    value.z80 |> set_flag_regs (adc value.value z80.flags))),
+          -- case 0x94: sub(HL>>>8); break;
+          -- case 0x94: sub(xy>>>8); break;
+          (0x94, (\ixiyhl z80 -> z80 |> set_flag_regs (z80_sub (get_h ixiyhl z80) z80.flags))),
+          -- case 0x95: sub(HL&0xFF); break;
+          -- case 0x95: sub(xy&0xFF); break;
+          (0x95, (\ixiyhl z80 -> z80 |> set_flag_regs (z80_sub (get_l ixiyhl z80) z80.flags))),
+          -- case 0x96: sub(env.mem(HL)); time+=3; break;
+          -- case 0x96: sub(env.mem(getd(xy))); time+=3; break;
+          (0x96, (\ixiyhl z80 -> let
+                                      value = hl_deref_with_z80 ixiyhl z80
+                                  in
+                                      value.z80 |> set_flag_regs (z80_sub value.value z80.flags)))
     ]
 
 execute_0x10: Z80 -> Z80
@@ -1615,28 +1637,6 @@ execute_0x3F z80 =
 executegt40ltC0: Int -> IXIYHL -> Z80 -> Z80
 executegt40ltC0 c ixiyhl z80 =
     case c of
-         -- case 0x90: sub(B); break;
-       0x90 -> z80 |> set_flag_regs (z80_sub z80.main.b z80.flags)
-         -- case 0x91: sub(C); break;
-       0x91 -> z80 |> set_flag_regs (z80_sub z80.main.c z80.flags)
-         -- case 0x92: sub(D); break;
-       0x92 -> z80 |> set_flag_regs (z80_sub z80.main.d z80.flags)
-         -- case 0x93: sub(E); break;
-       0x93 -> z80 |> set_flag_regs (z80_sub z80.main.e z80.flags)
-         -- case 0x94: sub(HL>>>8); break;
-         -- case 0x94: sub(xy>>>8); break;
-       0x94 -> z80 |> set_flag_regs (z80_sub (get_h ixiyhl z80) z80.flags)
-         -- case 0x95: sub(HL&0xFF); break;
-         -- case 0x95: sub(xy&0xFF); break;
-       0x95 -> z80 |> set_flag_regs (z80_sub (get_l ixiyhl z80) z80.flags)
-         -- case 0x96: sub(env.mem(HL)); time+=3; break;
-         -- case 0x96: sub(env.mem(getd(xy))); time+=3; break;
-       0x96 -> let
-                  value = hl_deref_with_z80 ixiyhl z80
-               in
-                  value.z80 |> set_flag_regs (z80_sub value.value z80.flags)
-         -- case 0x97: sub(A); break;
-       0x97 -> z80 |> set_flag_regs (z80_sub z80.flags.a z80.flags)
          -- case 0x98: sbc(B); break;
        0x98 -> z80 |> set_flag_regs (sbc z80.main.b z80.flags)
          -- case 0x99: sbc(C); break;
