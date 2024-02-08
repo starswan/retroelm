792bd6d3a5a7f772938cb847577da8ae0d95f5e0
starswan <github@starswan.com>
2024-02-08 21:29:51 +0000

    tidy up test

--- spec/features/z80_spec.rb
+++ spec/features/z80_spec.rb
@@ -7,32 +7,20 @@ RSpec.describe "Spectrum Emulator" do
     z80_game.save!
   end
 
-  let(:expected_hz) { (ENV['HZ'] || "9.3").to_f }
+  let(:expected_hz) { (ENV['HZ'] || "9.23").to_f }
 
+  # disable for now, as we ond't want to run the test twice really
   xcontext "with match day" do
     let(:z80_game) { build(:game, :match_day) }
 
     it "loads the emulator", :js do
       visit '/'
       click_on z80_game.name
-      # check that Elm is running
+
       expect(page).to have_content 'Refresh Interval'
-      sleep 5
-      # Test emulation speed in Hz
-      low = 0
-      high = page.find("#hz").text.to_f
-      # wait for speed to hit a steady state
-      while (high - low).abs > 0.01
-        times = 1.upto(6).map do
-          sleep 0.2
-          page.find("#hz").text.to_f
-        end
-        sorted = times.sort
-        low = sorted.first
-        high = sorted.last
-      end
-      expect(high).to be > expected_hz
-      p "Speed #{high} Hz"
+      speed = measure_speed_in_hz
+      expect(speed).to be > expected_hz
+      p "Speed #{speed} Hz"
     end
   end
 
@@ -60,23 +48,28 @@ RSpec.describe "Spectrum Emulator" do
     it "loads the emulator", :js do
       # check that Elm is running
       expect(page).to have_content 'Refresh Interval'
-      sleep 1
-      # Test emulation speed in Hz
-      low = 0
-      high = page.find("#hz").text.to_f
-      # wait for speed to hit a steady state
-      while (high - low).abs > 0.01
-        times = 1.upto(6).map do
-          sleep 0.2
-          page.find("#hz").text.to_f
-        end
-        sorted = times.sort
-        low = sorted.first
-        high = sorted.last
-      end
-      expect(high).to be > expected_hz
-      p "Speed #{high} Hz"
+      speed = measure_speed_in_hz
+      expect(speed).to be > expected_hz
+      p "Speed #{speed} Hz"
     end
+  end
+
+  def measure_speed_in_hz
+    sleep 1
+    # Test emulation speed in Hz
+    low = 0
+    high = page.find("#hz").text.to_f
+    # wait for speed to hit a steady state
+    while (high - low).abs > 0.01
+      times = 1.upto(6).map do
+        sleep 0.2
+        page.find("#hz").text.to_f
+      end
+      sorted = times.sort
+      low = sorted.first
+      high = sorted.last
+    end
+    high
   end
 
   def load_tapfile input_url, output_directory
