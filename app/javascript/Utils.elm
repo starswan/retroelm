589abf9cd9210d794f7648e48c82888a703e2d02
starswan <github@starswan.com>
2024-02-12 19:07:11 +0000

    elm-format utils

--- app/javascript/Utils.elm
+++ app/javascript/Utils.elm
@@ -1,6 +1,8 @@
 --
 -- $Id$
 --
+
+
 module Utils exposing (..)
 
 import Bitwise exposing (shiftLeftBy, shiftRightBy)
@@ -9,54 +11,73 @@ import Hex
 import Process
 import Task
 
-compact list = List.filterMap identity list
 
-shiftLeftBy8: Int -> Int
+compact list =
+    List.filterMap identity list
+
+
+shiftLeftBy8 : Int -> Int
 shiftLeftBy8 reg =
     shiftLeftBy 8 reg
 
-shiftLeftBy1: Int -> Int
+
+shiftLeftBy1 : Int -> Int
 shiftLeftBy1 reg =
     shiftLeftBy 1 reg
 
-shiftRightBy8: Int -> Int
+
+shiftRightBy8 : Int -> Int
 shiftRightBy8 reg =
     shiftRightBy 8 reg
 
-shiftRightBy1: Int -> Int
+
+shiftRightBy1 : Int -> Int
 shiftRightBy1 reg =
     shiftRightBy 1 reg
 
-listToDict: List Int -> Dict Int Int
+
+listToDict : List Int -> Dict Int Int
 listToDict intlist =
     let
-       x = List.indexedMap Tuple.pair intlist
+        x =
+            List.indexedMap Tuple.pair intlist
     in
-       Dict.fromList x
+    Dict.fromList x
 
-digitToString: Int -> Int -> String
+
+digitToString : Int -> Int -> String
 digitToString pad int =
-   int |> String.fromInt |> String.padLeft pad '0'
+    int |> String.fromInt |> String.padLeft pad '0'
 
-byte: Int -> Int
+
+byte : Int -> Int
 byte value =
-   if ((Bitwise.and value 0x80) /= 0) then
-      value - 256
-   else
-      value
+    if Bitwise.and value 0x80 /= 0 then
+        value - 256
 
-char: Int -> Int
+    else
+        value
+
+
+char : Int -> Int
 char value =
-   Bitwise.and value 0xFFFF
+    Bitwise.and value 0xFFFF
 
-toHexString: Int -> String
+
+toHexString : Int -> String
 toHexString value =
-   "0x" ++ (Hex.toString value |> String.toUpper |> (String.padLeft 4 '0'))
+    "0x" ++ (Hex.toString value |> String.toUpper |> String.padLeft 4 '0')
+
+
 
 -- just used for debugging
-toHexString2: Int -> String
+
+
+toHexString2 : Int -> String
 toHexString2 value =
-   "0x" ++ (Hex.toString value |> String.toUpper |> (String.padLeft 2 '0'))
+    "0x" ++ (Hex.toString value |> String.toUpper |> String.padLeft 2 '0')
+
+
 
 --https://stackoverflow.com/questions/40599512/how-to-achieve-behavior-of-settimeout-in-elm
 --delay : Float -> msg -> Cmd msg
@@ -71,7 +92,8 @@ toHexString2 value =
 --           -- returns it to our update function
 --           Task.perform identity
 
+
 delay : Float -> msg -> Cmd msg
 delay time msg =
-  Process.sleep time
-  |> Task.perform (\_ -> msg)
+    Process.sleep time
+        |> Task.perform (\_ -> msg)
