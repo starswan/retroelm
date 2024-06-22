8af26d9a79e3381862521cf35f8324af8619d991
starswan <github@starswan.com>
2024-06-22 22:55:24 +0100

    create cpu time ctime module

--- app/javascript/Z80Env.elm
+++ app/javascript/Z80Env.elm
@@ -5,17 +5,12 @@ module Z80Env exposing (..)
 
 import Array exposing (Array)
 import Bitwise exposing (and, or, shiftLeftBy, shiftRightBy)
+import CpuTimeCTime exposing (CpuTimeCTime, add_cpu_time_time, c_NOCONT, cont, cont1, cont_port)
 import Keyboard exposing (Keyboard, z80_keyboard_input)
 import Utils exposing (shiftLeftBy8, shiftRightBy8, toHexString)
 import Z80Ram exposing (Z80Ram, c_FRSTART, getRamValue)
 import Z80Rom exposing (Z80ROM, getROMValue, set_spectrum_rom)
 
-
-type alias CpuTimeCTime =
-    {
-            cpu_time: Int,
-            ctime: Int
-    }
 
 -- changing this to an array results in a recursion error in the browser :-(
 type alias Z80Env =
@@ -38,9 +33,6 @@ type alias ValueWithTime =
         value: Int,
         cpu_time: Int
     }
-
-c_NOCONT = 99999
-c_SCRENDT = 191*224+126
 
 z80env_constructor =
     Z80Env Z80Rom.constructor Z80Ram.constructor Keyboard.constructor (CpuTimeCTime c_FRSTART 0) 0
@@ -322,140 +314,6 @@ set_mem16 addr value z80env =
           z80env |> set_mem addr (Bitwise.and value 0xFF)
                  |> set_mem (addr + 1) (shiftRightBy8 value)
 
---	private final void cont1(int t) {
---		t += cpu.time;
---		if(t<0 || t>=SCRENDT) return;
---		if((t&7) >= 6) return;
---		if(t%224 < 126)
---			cpu.time += 6 - (t&7);
---	}
-cont1: Int -> CpuTimeCTime -> CpuTimeCTime
-cont1 tmp_t z80  =
-    let
-        t = tmp_t + z80.cpu_time
-    in
-        if (t<0) || (t>=c_SCRENDT) then
-           z80
-        else
-           if (Bitwise.and t 7) >= 6 then
-               z80
-           else
-               if (modBy 224 t) < 126 then
-                  z80 |> add_cpu_time_time (6 - (Bitwise.and t 7))
-               else
-                   z80
---
---	private final void cont(int n) {
---		int s, k;
---		int t = ctime;
---		if(t+n <= 0) return;
---		s = SCRENDT - t;
---		if(s < 0) return;
---		s %= 224;
---		if(s > 126) {
---			n -= s-126;
---			if(n <= 0) return;
---			t = 6; k = 15;
---		} else {
---			k = s>>>3;
---			s &= 7;
---			if(s == 7) {
---				s--;
---				if(--n == 0) return;
---			}
---			t = s;
---		}
---		n = n-1 >> 1;
---		if(k<n) n = k;
---		cpu.time += t + 6*n;
---	}
-
--- Helper implementation function for cont
-contimpl: Int -> Int -> CpuTimeCTime -> CpuTimeCTime
-contimpl tmp_n tmp_s z80env =
-    let
-        s = modBy 224 tmp_s
-        (ntk) = if s > 126 then
-                   { n = s - 126, t = 6, k = 15, o = False }
-                else
-                    let
-                        k2 = shiftRightBy 3 s
-                        s2 = Bitwise.and s 7
-                        (s3, n2, override) = if s2 == 7 then
-                        -- in (only) this branch we need to bale if n == 1
-                                                 (s2 - 1, tmp_n - 1, tmp_n == 1)
-                                              else
-                                                 (s2, tmp_n, False)
-                     in
-                        { n = n2, t = s3, k = k2, o = override }
-        n3 = shiftRightBy 1 (ntk.n - 1)
-        n4 = if ntk.k<n3 then
-                ntk.k
-             else
-                n3
-    in
-        if ntk.o then
-            z80env
-        else
-            { z80env | cpu_time = z80env.cpu_time + (ntk.t + 6*n4) }
-
-cont: Int -> CpuTimeCTime-> CpuTimeCTime
-cont tmp_n z80env =
-    let
-        tmp_t = z80env.ctime
-    in
-        if tmp_t + tmp_n <= 0 then
-            z80env
-        else
-            let
-                tmp_s = c_SCRENDT - tmp_t
-            in
-                if tmp_s < 0 then
-                    z80env
-                else
-                    z80env |> contimpl tmp_n tmp_s
-
---private void cont_port(int port)
---{
---	int n = cpu.time - ctime;
---	if(n>0) cont(n);
---
---	if((port&0xC000) != 0x4000) {
---		if((port&0x0001)==0)
---			cont1(1);
---		ctime = NOCONT;
---	} else {
---		ctime = cpu.time;
---		cont(2 + ((port&1)<<1));
---		ctime = cpu.time+4;
---	}
---}
-cont_port: Int -> CpuTimeCTime -> CpuTimeCTime
-cont_port portn z80env =
-   let
-      n = z80env.cpu_time - z80env.ctime
-      env1_time = if n > 0 then
-                     z80env |> cont n
-                  else
-                     z80env
-      env2 = if and portn 0xC000 /= 0x4000 then
-               let
-                  env3 = if and portn 0x0001 == 0 then
-                            env1_time |> cont1 1
-                         else
-                            env1_time
-               in
-                  { env3 | ctime = c_NOCONT }
-             else
-               let
-                   env3 = CpuTimeCTime env1_time.cpu_time env1_time.cpu_time
-                   contval = and portn 1 |> shiftLeftBy 1
-                   env4 = env3 |> cont (2 + contval)
-               in
-                   env4 |> add_cpu_time_time 4
-   in
-      env2
-
 cont_port_env: Int -> Z80Env -> Z80Env
 cont_port_env portn z80env =
     { z80env | time = z80env.time |> cont_port portn }
@@ -505,10 +363,6 @@ z80_in portnum env_in =
 add_cpu_time_env: Int -> Z80Env -> Z80Env
 add_cpu_time_env value z80env =
    { z80env | time = z80env.time |> add_cpu_time_time value }
-
-add_cpu_time_time: Int -> CpuTimeCTime -> CpuTimeCTime
-add_cpu_time_time value z80env =
-   { z80env | cpu_time = z80env.cpu_time + value }
 
 reset_cpu_time: Z80Env -> Z80Env
 reset_cpu_time z80env =
