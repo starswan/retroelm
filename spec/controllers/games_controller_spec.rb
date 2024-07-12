7d6f51758cbf7c6eadc48366b21f4d0717f7ec18
Stephen Dicks <github@starswan.com>
2024-07-12 17:57:22 +0100

    remove destructive actions from website (#211)

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -13,39 +13,8 @@ RSpec.describe GamesController, type: :controller do
     expect( assigns(:games)).not_to be_nil
   end
 
-  it "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  it "should create game" do
-    expect do
-      post :create, params: { :game => { name: 'game', tapfile: 'help.tap' } }
-    end.to change(Game, :count).by(1)
-
-    assert_redirected_to game_path(assigns(:game))
-  end
-
   it "should show game" do
     get :show, params: { :id => game_one.to_param }
     assert_response :success
-  end
-
-  it "should get edit" do
-    get :edit, params: { :id => game_one.to_param }
-    assert_response :success
-  end
-
-  it "should update game" do
-    put :update, params: { :id => game_one.to_param, :game => { tapfile: 'help.tap' } }
-    assert_redirected_to game_path(assigns(:game))
-  end
-
-  it "should destroy game" do
-    expect do
-      delete :destroy, params: { :id => game_one.to_param }
-    end.to change(Game, :count).by(-1)
-
-    assert_redirected_to games_path
   end
 end
