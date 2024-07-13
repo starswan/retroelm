2ac0c4834c8c3fba927eb74d3e0d60ea17ee0677
starswan <github@starswan.com>
2024-07-13 18:21:28 +0100

    tidy up and format new modules

--- app/javascript/Z80Types.elm
+++ app/javascript/Z80Types.elm
@@ -5,71 +5,86 @@ import CpuTimeCTime exposing (CpuTimeAndPc, CpuTimeCTime, CpuTimePcAndValue, add
 import Utils exposing (byte, char, shiftLeftBy8, shiftRightBy8)
 import Z80Env exposing (Z80Env, Z80EnvWithPC, add_cpu_time_env, mem, mem16, set_mem, z80_push)
 import Z80Flags exposing (FlagRegisters)
-type alias MainRegisters =
-   {
-      b:   Int,
-      c:   Int,
-      d:   Int,
-      e:   Int,
-      hl:  Int
-   }
-type alias MainWithIndexRegisters =
-   {
-      b:   Int,
-      c:   Int,
-      d:   Int,
-      e:   Int,
-      hl:  Int,
-      ix:  Int,
-      iy:  Int
-   }
 
-type alias ProgramCounter =
-    {
-        pc: Int
+
+type alias MainRegisters =
+    { b : Int
+    , c : Int
+    , d : Int
+    , e : Int
+    , hl : Int
     }
 
+
+type alias MainWithIndexRegisters =
+    { b : Int
+    , c : Int
+    , d : Int
+    , e : Int
+    , hl : Int
+    , ix : Int
+    , iy : Int
+    }
+
+
+type alias ProgramCounter =
+    { pc : Int
+    }
+
+
 type alias InterruptRegisters =
-   {
-      ir:  Int,
-      r:   Int,
-      --mp:  Int, -- /* MEMPTR, the hidden register emulated according to memptr_eng.txt */
-      iff: Int,
-      iM: Int,
-      halted: Bool
-   }
+    { ir : Int
+    , r : Int
+    , --mp:  Int, -- /* MEMPTR, the hidden register emulated according to memptr_eng.txt */
+      iff : Int
+    , iM : Int
+    , halted : Bool
+    }
+
 
 type alias Z80 =
-   {
-      env: Z80Env,
-      pc:  Int,
-      main: MainWithIndexRegisters,
-      flags: FlagRegisters,
-      alt_main: MainRegisters,
-      alt_flags: FlagRegisters,
-      interrupts: InterruptRegisters,
-      time_limit: Int
-   }
+    { env : Z80Env
+    , pc : Int
+    , main : MainWithIndexRegisters
+    , flags : FlagRegisters
+    , alt_main : MainRegisters
+    , alt_flags : FlagRegisters
+    , interrupts : InterruptRegisters
+    , time_limit : Int
+    }
+
 
 type alias EnvWithPCAndValue =
-   {
-        env: Z80Env,
-        pc: Int,
-        value: Int
-   }
+    { env : Z80Env
+    , pc : Int
+    , value : Int
+    }
+
 
 type alias IntWithFlagsTimeAndPC =
-  {
-     value: Int,
-     flags: FlagRegisters,
-     time: CpuTimeCTime,
-     pc: Int
-  }
+    { value : Int
+    , flags : FlagRegisters
+    , time : CpuTimeCTime
+    , pc : Int
+    }
+
+
 
 -- Think this could be a useful parameter to execute_lt40 to avoid the duplication
 -- problem currently being experienced in function group_xy
-type IXIYHL = IX | IY | HL
-type IXIY = IXIY_IX | IXIY_IY
+
+
+type IXIYHL
+    = IX
+    | IY
+    | HL
+
+
+type IXIY
+    = IXIY_IX
+    | IXIY_IY
+
+
 
 --
 --	private int imm8()
@@ -79,14 +94,23 @@ type IXIY = IXIY_IX | IXIY_IY
 --		time += 3;
 --		return v;
 --	}
-imm8: Z80 -> CpuTimePcAndValue
+
+
+imm8 : Z80 -> CpuTimePcAndValue
 imm8 z80 =
     let
-        v = mem z80.pc z80.env
-        new_pc = Bitwise.and (z80.pc + 1) 0xFFFF
-        env_1 = v.time |> add_cpu_time_time 3
+        v =
+            mem z80.pc z80.env
+
+        new_pc =
+            Bitwise.and (z80.pc + 1) 0xFFFF
+
+        env_1 =
+            v.time |> add_cpu_time_time 3
     in
-        CpuTimePcAndValue env_1 new_pc v.value
+    CpuTimePcAndValue env_1 new_pc v.value
+
+
 
 -- would need the side-effect of mem call as well
 --imm8_discard: Z80 -> Z80
@@ -99,255 +123,375 @@ imm8 z80 =
 --		time += 6;
 --		return v;
 --	}
-imm16: Z80 -> CpuTimePcAndValue
+
+
+imm16 : Z80 -> CpuTimePcAndValue
 imm16 z80 =
     let
-        v = mem16 z80.pc z80.env
-        pc = Bitwise.and (z80.pc + 2) 0xFFFF
-        env = v.time |> add_cpu_time_time 6
+        v =
+            mem16 z80.pc z80.env
+
+        pc =
+            Bitwise.and (z80.pc + 2) 0xFFFF
+
+        env =
+            v.time |> add_cpu_time_time 6
     in
-        CpuTimePcAndValue env pc v.value
+    CpuTimePcAndValue env pc v.value
+
+
 
 --	private void jp(boolean y)
 --	{
 --		int a = MP = imm16();
 --		if(y) PC = a;
 --	}
-jp_z80: Bool -> Z80 -> Z80
-jp_z80 y z80 =
-   let
-      a = imm16 z80
-      env = z80.env
-      z80_1 = { z80 | pc = a.pc, env = { env | time = a.time } }
-   in
-      if y then
-         { z80_1 | pc = a.value }
-      else
-         z80_1
 
-jp: Bool -> Z80 -> CpuTimeAndPc
-jp y z80 =
-  let
-     a = imm16 z80
-  in
+
+jp_z80 : Bool -> Z80 -> Z80
+jp_z80 y z80 =
+    let
+        a =
+            imm16 z80
+
+        env =
+            z80.env
+
+        z80_1 =
+            { z80 | pc = a.pc, env = { env | time = a.time } }
+    in
     if y then
-      CpuTimeAndPc a.time a.value
+        { z80_1 | pc = a.value }
+
     else
-      CpuTimeAndPc a.time a.pc
+        z80_1
+
+
+jp : Bool -> Z80 -> CpuTimeAndPc
+jp y z80 =
+    let
+        a =
+            imm16 z80
+    in
+    if y then
+        CpuTimeAndPc a.time a.value
+
+    else
+        CpuTimeAndPc a.time a.pc
+
+
 
 --	private void call(boolean y)
 --	{
 --		int a = MP = imm16();
 --		if(y) {push(PC); PC = a;}
 --	}
-call_z80: Bool -> Z80 -> Z80
-call_z80 y z80 =
-   let
-      a = imm16 z80
-      env = z80.env
-      z80_2 = { z80 | pc = a.pc, env = { env | time = a.time } }
-   in
-     if y then
-      let
-         --b = debug_log "call" (a.value |> subName) Nothing
-         --z80_1 = z80_2 |> push z80_2.pc |> set_pc a.value
-         pushed = z80_2.env |> z80_push z80_2.pc
-         z80_1 = { z80_2 | env = pushed, pc = a.value }
-      in
-         z80_1
-     else
-       z80_2
+--call_z80: Bool -> Z80 -> Z80
+--call_z80 y z80 =
+--   let
+--      a = imm16 z80
+--      env = z80.env
+--      z80_2 = { z80 | pc = a.pc, env = { env | time = a.time } }
+--   in
+--     if y then
+--      let
+--         --b = debug_log "call" (a.value |> subName) Nothing
+--         --z80_1 = z80_2 |> push z80_2.pc |> set_pc a.value
+--         pushed = z80_2.env |> z80_push z80_2.pc
+--         z80_1 = { z80_2 | env = pushed, pc = a.value }
+--      in
+--         z80_1
+--     else
+--       z80_2
 
-call: Bool -> Z80 -> Z80EnvWithPC
+
+call : Bool -> Z80 -> Z80EnvWithPC
 call y z80 =
-   let
-      a = imm16 z80
-      env = z80.env
-      z80_2 = { z80 | pc = a.pc, env = { env | time = a.time } }
-   in
-     if y then
-      let
-         --b = debug_log "call" (a.value |> subName) Nothing
-         --z80_1 = z80_2 |> push z80_2.pc |> set_pc a.value
-         pushed = z80_2.env |> z80_push z80_2.pc
-         --z80_1 = { z80_2 | env = pushed, pc = a.value }
-      in
-         Z80EnvWithPC pushed a.value
-     else
-       Z80EnvWithPC z80_2.env a.pc
+    let
+        a =
+            imm16 z80
 
-rst_z80: Int -> Z80 -> Z80
+        env =
+            z80.env
+
+        z80_2 =
+            { z80 | pc = a.pc, env = { env | time = a.time } }
+    in
+    if y then
+        let
+            --b = debug_log "call" (a.value |> subName) Nothing
+            --z80_1 = z80_2 |> push z80_2.pc |> set_pc a.value
+            pushed =
+                z80_2.env |> z80_push z80_2.pc
+
+            --z80_1 = { z80_2 | env = pushed, pc = a.value }
+        in
+        Z80EnvWithPC pushed a.value
+
+    else
+        Z80EnvWithPC z80_2.env a.pc
+
+
+rst_z80 : Int -> Z80 -> Z80
 rst_z80 c z80 =
     --z80 |> push z80.pc |> set_pc (c - 199)
     let
-        pushed  = z80.env |> z80_push z80.pc
+        pushed =
+            z80.env |> z80_push z80.pc
     in
-        { z80 | env = pushed, pc = (c - 199) }
+    { z80 | env = pushed, pc = c - 199 }
 
-rst: Int -> Z80 -> Z80EnvWithPC
+
+rst : Int -> Z80 -> Z80EnvWithPC
 rst c z80 =
-   --z80 |> push z80.pc |> set_pc (c - 199)
-   let
-     pushed  = z80.env |> z80_push z80.pc
-   in
-     Z80EnvWithPC pushed (c - 199)
+    --z80 |> push z80.pc |> set_pc (c - 199)
+    let
+        pushed =
+            z80.env |> z80_push z80.pc
+    in
+    Z80EnvWithPC pushed (c - 199)
 
-a_with_z80: Z80 -> CpuTimePcAndValue
+
+a_with_z80 : Z80 -> CpuTimePcAndValue
 a_with_z80 z80 =
     CpuTimePcAndValue z80.env.time z80.pc z80.flags.a
 
-add_cpu_time: Int -> Z80 -> Z80
-add_cpu_time value z80 =
-   let
-      env = z80.env |> add_cpu_time_env value
-   in
-      { z80 | env = env }
 
-b_with_z80: Z80 -> CpuTimePcAndValue
+add_cpu_time : Int -> Z80 -> Z80
+add_cpu_time value z80 =
+    let
+        env =
+            z80.env |> add_cpu_time_env value
+    in
+    { z80 | env = env }
+
+
+b_with_z80 : Z80 -> CpuTimePcAndValue
 b_with_z80 z80 =
     CpuTimePcAndValue z80.env.time z80.pc z80.main.b
 
-c_with_z80: Z80 -> CpuTimePcAndValue
+
+c_with_z80 : Z80 -> CpuTimePcAndValue
 c_with_z80 z80 =
     CpuTimePcAndValue z80.env.time z80.pc z80.main.c
 
-d_with_z80: Z80 -> CpuTimePcAndValue
+
+d_with_z80 : Z80 -> CpuTimePcAndValue
 d_with_z80 z80 =
     CpuTimePcAndValue z80.env.time z80.pc z80.main.d
 
-e_with_z80: Z80 -> CpuTimePcAndValue
+
+e_with_z80 : Z80 -> CpuTimePcAndValue
 e_with_z80 z80 =
     CpuTimePcAndValue z80.env.time z80.pc z80.main.e
 
-get_ixiy_xy: IXIY -> MainWithIndexRegisters -> Int
-get_ixiy_xy ixiy z80_main =
-   case ixiy of
-      IXIY_IX -> z80_main.ix
-      IXIY_IY -> z80_main.iy
 
-h_with_z80: IXIYHL -> Z80 -> CpuTimePcAndValue
+get_ixiy_xy : IXIY -> MainWithIndexRegisters -> Int
+get_ixiy_xy ixiy z80_main =
+    case ixiy of
+        IXIY_IX ->
+            z80_main.ix
+
+        IXIY_IY ->
+            z80_main.iy
+
+
+h_with_z80 : IXIYHL -> Z80 -> CpuTimePcAndValue
 h_with_z80 ixiyhl z80 =
     CpuTimePcAndValue z80.env.time z80.pc (shiftRightBy8 (get_xy ixiyhl z80.main))
 
-hl_deref_with_z80: IXIYHL -> Z80 -> CpuTimePcAndValue
+
+hl_deref_with_z80 : IXIYHL -> Z80 -> CpuTimePcAndValue
 hl_deref_with_z80 ixiyhl z80 =
     let
-        a = env_mem_hl ixiyhl z80
-        new_b = mem a.value z80.env
+        a =
+            env_mem_hl ixiyhl z80
+
+        new_b =
+            mem a.value z80.env
     in
-        CpuTimePcAndValue new_b.time a.pc new_b.value
+    CpuTimePcAndValue new_b.time a.pc new_b.value
 
-inc_pc: Z80 -> Int
+
+inc_pc : Z80 -> Int
 inc_pc z80 =
-   Bitwise.and (z80.pc + 1) 0xFFFF
+    Bitwise.and (z80.pc + 1) 0xFFFF
 
-inc_pc2: Z80 -> Int
+
+inc_pc2 : Z80 -> Int
 inc_pc2 z80 =
-   Bitwise.and (z80.pc + 2) 0xFFFF
+    Bitwise.and (z80.pc + 2) 0xFFFF
 
-l_with_z80: IXIYHL -> Z80 -> CpuTimePcAndValue
+
+l_with_z80 : IXIYHL -> Z80 -> CpuTimePcAndValue
 l_with_z80 ixiyhl z80 =
     CpuTimePcAndValue z80.env.time z80.pc (Bitwise.and (get_xy ixiyhl z80.main) 0xFF)
 
+
+
 -- There appear to be many situations where we already know that we don't need all
 -- this complexity as we're just doing LD A,B or something similar - so stop using it in those cases
-load408bit: Int -> IXIYHL -> Z80 -> CpuTimePcAndValue
+
+
+load408bit : Int -> IXIYHL -> Z80 -> CpuTimePcAndValue
 load408bit c_value ixiyhl z80 =
-   case (Bitwise.and c_value 0x07) of
-      0 -> b_with_z80 z80
-      1 -> c_with_z80 z80
-      2 -> d_with_z80 z80
-      3 -> e_with_z80 z80
-      4 -> h_with_z80 ixiyhl z80
-      5 -> l_with_z80 ixiyhl z80
-      6 -> hl_deref_with_z80 ixiyhl z80
-      _ -> a_with_z80 z80
+    case Bitwise.and c_value 0x07 of
+        0 ->
+            b_with_z80 z80
 
-set408bit: Int -> Int -> IXIYHL -> Z80 -> Z80
+        1 ->
+            c_with_z80 z80
+
+        2 ->
+            d_with_z80 z80
+
+        3 ->
+            e_with_z80 z80
+
+        4 ->
+            h_with_z80 ixiyhl z80
+
+        5 ->
+            l_with_z80 ixiyhl z80
+
+        6 ->
+            hl_deref_with_z80 ixiyhl z80
+
+        _ ->
+            a_with_z80 z80
+
+
+set408bit : Int -> Int -> IXIYHL -> Z80 -> Z80
 set408bit c value ixiyhl z80 =
-    case (Bitwise.and c 0x07) of
-       0 -> z80 |> set_b value
-       1 -> z80 |> set_c value
-       2 -> z80 |> set_d value
-       3 -> z80 |> set_e value
-       4 -> set_h_z80 value ixiyhl z80
-       5 -> set_l_z80 value ixiyhl z80
-       6 -> { z80 | env = set_mem z80.main.hl value z80.env }
-       _ -> z80 |> set_a value
+    case Bitwise.and c 0x07 of
+        0 ->
+            z80 |> set_b value
 
-set_b: Int -> Z80 -> Z80
+        1 ->
+            z80 |> set_c value
+
+        2 ->
+            z80 |> set_d value
+
+        3 ->
+            z80 |> set_e value
+
+        4 ->
+            set_h_z80 value ixiyhl z80
+
+        5 ->
+            set_l_z80 value ixiyhl z80
+
+        6 ->
+            { z80 | env = set_mem z80.main.hl value z80.env }
+
+        _ ->
+            z80 |> set_a value
+
+
+set_b : Int -> Z80 -> Z80
 set_b value z80 =
-   let
-      z80_main = z80.main
-   in
+    let
+        z80_main =
+            z80.main
+    in
     { z80 | main = { z80_main | b = value } }
 
-set_c: Int -> Z80 -> Z80
+
+set_c : Int -> Z80 -> Z80
 set_c value z80 =
-   let
-      z80_main = z80.main
-   in
+    let
+        z80_main =
+            z80.main
+    in
     { z80 | main = { z80_main | c = value } }
 
-set_d: Int -> Z80 -> Z80
+
+set_d : Int -> Z80 -> Z80
 set_d value z80 =
-   let
-      z80_main = z80.main
-   in
+    let
+        z80_main =
+            z80.main
+    in
     { z80 | main = { z80_main | d = value } }
 
-set_e: Int -> Z80 -> Z80
+
+set_e : Int -> Z80 -> Z80
 set_e value z80 =
-   let
-      z80_main = z80.main
-   in
+    let
+        z80_main =
+            z80.main
+    in
     { z80 | main = { z80_main | e = value } }
 
-set_flag_regs: FlagRegisters -> Z80 -> Z80
+
+set_flag_regs : FlagRegisters -> Z80 -> Z80
 set_flag_regs flags z80 =
-   { z80 | flags = flags }
+    { z80 | flags = flags }
 
-set_h: Int -> IXIYHL -> MainWithIndexRegisters -> MainWithIndexRegisters
+
+set_h : Int -> IXIYHL -> MainWithIndexRegisters -> MainWithIndexRegisters
 set_h value ixiyhl z80 =
-   let
-      xy = get_xy ixiyhl z80
-   in
-      set_xy (Bitwise.or (Bitwise.and xy 0xFF) (shiftLeftBy8 value)) ixiyhl z80
+    let
+        xy =
+            get_xy ixiyhl z80
+    in
+    set_xy (Bitwise.or (Bitwise.and xy 0xFF) (shiftLeftBy8 value)) ixiyhl z80
 
-set_l: Int -> IXIYHL -> MainWithIndexRegisters -> MainWithIndexRegisters
+
+set_l : Int -> IXIYHL -> MainWithIndexRegisters -> MainWithIndexRegisters
 set_l value ixiyhl z80 =
-   let
-     xy = get_xy ixiyhl z80
-   in
-     set_xy (Bitwise.or (Bitwise.and xy 0xFF00) value) ixiyhl z80
+    let
+        xy =
+            get_xy ixiyhl z80
+    in
+    set_xy (Bitwise.or (Bitwise.and xy 0xFF00) value) ixiyhl z80
 
-set_h_z80: Int -> IXIYHL -> Z80 -> Z80
+
+set_h_z80 : Int -> IXIYHL -> Z80 -> Z80
 set_h_z80 value ixiyhl z80 =
     let
-        main = z80.main |> set_h value ixiyhl
+        main =
+            z80.main |> set_h value ixiyhl
     in
-        { z80 | main = main }
+    { z80 | main = main }
 
-set_l_z80: Int -> IXIYHL -> Z80 -> Z80
+
+set_l_z80 : Int -> IXIYHL -> Z80 -> Z80
 set_l_z80 value ixiyhl z80 =
     let
-        main = z80.main |> set_l value ixiyhl
+        main =
+            z80.main |> set_l value ixiyhl
     in
-        { z80 | main = main }
+    { z80 | main = main }
 
-get_xy: IXIYHL -> MainWithIndexRegisters -> Int
+
+get_xy : IXIYHL -> MainWithIndexRegisters -> Int
 get_xy ixiyhl z80_main =
-   case ixiyhl of
-      IX -> z80_main.ix
-      IY -> z80_main.iy
-      HL -> z80_main.hl
+    case ixiyhl of
+        IX ->
+            z80_main.ix
 
-set_xy: Int -> IXIYHL -> MainWithIndexRegisters -> MainWithIndexRegisters
+        IY ->
+            z80_main.iy
+
+        HL ->
+            z80_main.hl
+
+
+set_xy : Int -> IXIYHL -> MainWithIndexRegisters -> MainWithIndexRegisters
 set_xy value ixiyhl z80 =
-   case ixiyhl of
-      IX -> { z80 | ix = value }
-      IY -> { z80 | iy = value }
-      HL -> { z80 | hl = value }
+    case ixiyhl of
+        IX ->
+            { z80 | ix = value }
+
+        IY ->
+            { z80 | iy = value }
+
+        HL ->
+            { z80 | hl = value }
+
+
 
 --
 --	private int getd(int xy)
@@ -363,28 +507,33 @@ set_xy value ixiyhl z80 =
 --      d = z80.env |> mem z80.pc
 --   in
 --      CpuTimePcAndValue (d.time |> add_cpu_time_time 8) (char (z80.pc + 1)) (char (xy + byte d.value))
-env_mem_hl: IXIYHL -> Z80 -> CpuTimePcAndValue
-env_mem_hl ixiyhl z80 =
-  case ixiyhl of
-    HL -> CpuTimePcAndValue z80.env.time z80.pc z80.main.hl
-    IX ->
-      let
-        dval = z80.env |> mem z80.pc
-      in
-        CpuTimePcAndValue (dval.time |> add_cpu_time_time 8) (char (z80.pc + 1)) (char (z80.main.ix + byte dval.value))
-    IY ->
-      let
-        dval = z80.env |> mem z80.pc
-      in
-        CpuTimePcAndValue (dval.time |> add_cpu_time_time 8) (char (z80.pc + 1)) (char (z80.main.iy + byte dval.value))
 
-set_a: Int -> Z80 -> Z80
+
+env_mem_hl : IXIYHL -> Z80 -> CpuTimePcAndValue
+env_mem_hl ixiyhl z80 =
+    case ixiyhl of
+        HL ->
+            CpuTimePcAndValue z80.env.time z80.pc z80.main.hl
+
+        IX ->
+            let
+                dval =
+                    z80.env |> mem z80.pc
+            in
+            CpuTimePcAndValue (dval.time |> add_cpu_time_time 8) (char (z80.pc + 1)) (char (z80.main.ix + byte dval.value))
+
+        IY ->
+            let
+                dval =
+                    z80.env |> mem z80.pc
+            in
+            CpuTimePcAndValue (dval.time |> add_cpu_time_time 8) (char (z80.pc + 1)) (char (z80.main.iy + byte dval.value))
+
+
+set_a : Int -> Z80 -> Z80
 set_a value z80 =
     let
-        z80_flags = z80.flags
+        z80_flags =
+            z80.flags
     in
-       { z80 | flags = { z80_flags | a = value } }
-
-
-
-
+    { z80 | flags = { z80_flags | a = value } }
