0e48b38c9400b993861312411519c4bd0e1df3c7
starswan <github@starswan.com>
2024-08-16 16:28:53 +0100

    change seed to football manager rather than match day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Game.create! :name => 'Match Day', :tapfile => 'MATCHDAY.tap'
+# Game.create! :name => 'Match Day', :tapfile => 'MATCHDAY.tap'
+Game.create! :name => 'Football Manager', :tapfile => 'FOOTMANG.tap'
 Game.create! :name => 'Full Z80 Test', :tapfile => 'z80test/z80full.tap'
