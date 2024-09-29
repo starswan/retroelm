fb645bc5e0328d88cfb6e7092f4d0b8da780e813
starswan <github@starswan.com>
2024-09-29 09:34:55 +0100

    INdexes for screen data

--- app/javascript/ScreenStorage.elm
+++ app/javascript/ScreenStorage.elm
@@ -1,21 +1,45 @@
 module ScreenStorage exposing (..)
 
--- Convert row index into start row data location
-
 import Bitwise exposing (shiftLeftBy, shiftRightBy)
-import Z80Memory exposing (Z80Memory, getMemValue)
+import Vector24 exposing (Vector24)
+import Vector32 exposing (Vector32)
+import Vector8 exposing (Vector8)
 
 
 range07 =
-    List.range 0 7
+    [ Vector24.Index0
+    , Vector24.Index1
+    , Vector24.Index2
+    , Vector24.Index3
+    , Vector24.Index4
+    , Vector24.Index5
+    , Vector24.Index6
+    , Vector24.Index7
+    ]
 
 
 range8_15 =
-    List.range 8 15
+    [ Vector24.Index8
+    , Vector24.Index9
+    , Vector24.Index10
+    , Vector24.Index11
+    , Vector24.Index12
+    , Vector24.Index13
+    , Vector24.Index14
+    , Vector24.Index15
+    ]
 
 
 range16_23 =
-    List.range 16 23
+    [ Vector24.Index16
+    , Vector24.Index17
+    , Vector24.Index18
+    , Vector24.Index19
+    , Vector24.Index20
+    , Vector24.Index21
+    , Vector24.Index22
+    , Vector24.Index23
+    ]
 
 
 bank0_attr_indexes =
@@ -34,16 +58,29 @@ attr_indexes =
     bank0_attr_indexes ++ bank1_attr_indexes ++ bank2_attr_indexes
 
 
-dataOffsets =
-    range0_191 |> List.map calcDataOffset
+type alias RowIndex =
+    { row : Vector24.Index
+    , subRow : Vector8.Index
+    }
+
+
+
+--dataOffsets =
+--    range0_191 |> List.map calcDataOffset
 
 
 range0_191 =
     List.range 0 191
 
 
+type alias ScreenRow =
+    { data : Vector8 (Vector32 Int)
+    , attributes : Vector32 Int
+    }
+
+
 type alias Z80Screen =
-    { screen : Z80Memory
+    { screenData : Vector24 ScreenRow
     , border : Int
 
     --flash: Int,
@@ -68,21 +105,31 @@ constructor : Z80Screen
 constructor =
     let
         --for(int i=6144;i<6912;i++) ram[i] = 070; // white
-        screen_data =
-            List.repeat 6144 0
+        --screen_data =
+        --    List.repeat 6144 0 |> Z80Memory.constructor
+        screenLine =
+            Vector32.repeat 0
 
-        attributes =
-            List.repeat 768 0x38
+        screenVec =
+            Vector8.repeat screenLine
 
-        -- white
-        screen =
-            List.concat [ screen_data, attributes ] |> Z80Memory.constructor
+        attrline =
+            Vector32.repeat 0x38
+
+        row =
+            ScreenRow screenVec attrline
+
+        screenData =
+            Vector24.repeat row
     in
-    --Z80Screen screen 7 0 0 0 0 0
-    Z80Screen screen 7
+    Z80Screen screenData 7
 
 
-calcDataOffset : Int -> Int
+
+-- Convert row index into start row data location
+
+
+calcDataOffset : Int -> ( Vector24.Index, Vector8.Index )
 calcDataOffset start =
     let
         bankStart =
@@ -93,57 +140,192 @@ calcDataOffset start =
 
         data_offset =
             start |> Bitwise.and 0x07 |> shiftLeftBy 3
+
+        offset =
+            bankStart + bankOffset + data_offset
+
+        row =
+            Vector24.intToIndex (offset // 8) |> Maybe.withDefault Vector24.Index0
+
+        subRow =
+            Vector8.intToIndex (offset |> remainderBy 8) |> Maybe.withDefault Vector8.Index0
     in
-    bankStart + bankOffset + data_offset
+    ( row, subRow )
 
 
 screenOffsets =
     attr_indexes |> List.indexedMap (\index attr_index -> ( calcDataOffset index, attr_index ))
 
 
-range031 =
-    List.range 0 31
-
-mapScreen : ( Int, Int ) -> Z80Screen -> Int -> RawScreenData
-mapScreen ( row_index, attr_index ) z80_screen index =
+mapScreen : ( ( Vector24.Index, Vector8.Index ), Vector24.Index ) -> Z80Screen -> Vector32.Index -> RawScreenData
+mapScreen ( ( row_index, subrow_index ), attr_index ) z80_screen index =
     let
-        row_offset =
-            row_index * 32
-
-        attr_offset =
-            0x1800 + (attr_index * 32)
-
         data =
-            getScreenValue (row_offset + index) z80_screen
+            z80_screen |> getScreenDataValue row_index subrow_index index
 
         colour =
-            getScreenValue (attr_offset + index) z80_screen
+            z80_screen |> getScreenAttributeValue attr_index index
     in
     { colour = colour, data = data }
 
 
-setScreenValue : Int -> Int -> Z80Screen -> Z80Screen
-setScreenValue addr value z80s =
+attrIndex : Int -> ( Vector24.Index, Vector32.Index )
+attrIndex address =
     let
-        z80screen =
-            z80s |> refresh_screen
+        rowLine =
+            address // 32 |> Vector24.intToIndex |> Maybe.withDefault Vector24.Index0
+
+        colLine =
+            address |> remainderBy 32 |> Vector32.intToIndex |> Maybe.withDefault Vector32.Index0
     in
-    { z80screen | screen = z80screen.screen |> Z80Memory.setMemValue addr value }
+    ( rowLine, colLine )
+
+
+dataIndex : Int -> ( Vector24.Index, Vector8.Index, Vector32.Index )
+dataIndex address =
+    let
+        topPart =
+            address // 32
+
+        rowLine =
+            topPart // 8 |> Vector24.intToIndex |> Maybe.withDefault Vector24.Index0
+
+        midLine =
+            topPart |> remainderBy 8 |> Vector8.intToIndex |> Maybe.withDefault Vector8.Index0
+
+        col =
+            (address |> remainderBy 32) |> Vector32.intToIndex |> Maybe.withDefault Vector32.Index0
+    in
+    ( rowLine, midLine, col )
+
+
+setScreenValue : Int -> Int -> Z80Screen -> Z80Screen
+setScreenValue addr value z80screen =
+    if addr < 0x1800 then
+        setScreenDataValue addr value z80screen
+
+    else
+        setScreenAttributeValue (addr - 0x1800) value z80screen
+
+
+setScreenDataValue : Int -> Int -> Z80Screen -> Z80Screen
+setScreenDataValue addr value z80screen =
+    let
+        ( rowAddress, midAddress, columnAddress ) =
+            dataIndex addr
+
+        row =
+            z80screen.screenData |> Vector24.get rowAddress
+
+        newMid =
+            row.data |> Vector8.get midAddress |> Vector32.set columnAddress value
+
+        newScreenRow =
+            { row | data = row.data |> Vector8.set midAddress newMid }
+    in
+    { z80screen | screenData = z80screen.screenData |> Vector24.set rowAddress newScreenRow }
+
+
+setScreenAttributeValue : Int -> Int -> Z80Screen -> Z80Screen
+setScreenAttributeValue addr value z80screen =
+    let
+        ( rowAddress, columnAddress ) =
+            attrIndex addr
+
+        row =
+            z80screen.screenData |> Vector24.get rowAddress
+
+        newScreenRow =
+            { row | attributes = row.attributes |> Vector32.set columnAddress value }
+    in
+    { z80screen | screenData = z80screen.screenData |> Vector24.set rowAddress newScreenRow }
 
 
 getScreenValue : Int -> Z80Screen -> Int
 getScreenValue addr screen =
-    screen.screen |> getMemValue addr
+    if addr < 0x1800 then
+        let
+            ( rowAddress, mid, column ) =
+                dataIndex addr
+        in
+        screen |> getScreenDataValue rowAddress mid column
+
+    else
+        let
+            ( rowAddress, column ) =
+                attrIndex (addr - 0x1800)
+        in
+        screen |> getScreenAttributeValue rowAddress column
 
 
-rawScreenData : Z80Screen -> List (List RawScreenData)
+getScreenDataValue : Vector24.Index -> Vector8.Index -> Vector32.Index -> Z80Screen -> Int
+getScreenDataValue rowAddress subRowIndex column screen =
+    screen.screenData |> Vector24.get rowAddress |> .data |> Vector8.get subRowIndex |> Vector32.get column
+
+
+getScreenAttributeValue : Vector24.Index -> Vector32.Index -> Z80Screen -> Int
+getScreenAttributeValue rowAddress column screen =
+    screen.screenData |> Vector24.get rowAddress |> .attributes |> Vector32.get column
+
+
+rawScreenData : Z80Screen -> List (Vector32 RawScreenData)
 rawScreenData z80_screen =
     screenOffsets
         |> List.map
             (\line_num ->
-                range031 |> List.map (mapScreen line_num z80_screen)
+                Vector32.indices |> Vector32.map (mapScreen line_num z80_screen)
             )
 
-refresh_screen : Z80Screen -> Z80Screen
-refresh_screen z80env =
-    z80env
+
+
+--refresh_screen : Z80Screen -> Z80Screen
+--refresh_screen z80env =
+--    z80env
+--private final void refresh_screen() {
+--	int ft = cpu.time;
+--	if(ft < refrs_t)
+--		return;
+--	final int flash = this.flash;
+--	int a = refrs_a, b = refrs_b;
+--	int t = refrs_t, s = refrs_s;
+--	do {
+--		int sch = 0;
+--
+--		int v = ram[a]<<8 | ram[b++];
+--		if(v>=0x8000) v ^= flash;
+--		v = canonic[v];
+--		if(v!=screen[s]) {
+--			screen[s] = v;
+--			sch = 1;
+--		}
+--
+--		v = ram[a+1]<<8 | ram[b++];
+--		if(v>=0x8000) v ^= flash;
+--		v = canonic[v];
+--		if(v!=screen[++s]) {
+--			screen[s] = v;
+--			sch += 2;
+--		}
+--
+--		if(sch!=0)
+--			scrchg[a-0x1800>>5] |= sch<<(a&31);
+--
+--		a+=2; t+=8; s++;
+--		if((a&31)!=0) continue;
+--		// next line
+--		t+=96; s+=2*Mh;
+--		a-=32; b+=256-32;
+--		if((b&0x700)!=0) continue;
+--		// next row
+--		a+=32; b+=32-0x800;
+--		if((b&0xE0)!=0) continue;
+--		// next segment
+--		b+=0x800-256;
+--		if(b>=6144) {
+--			t = REFRESH_END;
+--			break;
+--		}
+--	} while(ft >= t);
+--	refrs_a = a; refrs_b = b;
+--	refrs_t = t; refrs_s = s;
+--}
--- tests/ScreenStorageTest.elm
+++ tests/ScreenStorageTest.elm
@@ -3,77 +3,83 @@ module ScreenStorageTest exposing (..)
 import Expect
 import ScreenStorage exposing (screenOffsets)
 import Test exposing (Test, describe, test)
+import Vector24
+import Vector8
 suite : Test
 suite =
    -- The Spectrum screen is mapped as 3 banks of 8 lines. The lines are scanned
    -- in banks of 8 so memory location 32 is at the start of line 8
    describe "things" [
+      let
+          numericOffsets = screenOffsets
+            |> List.map (\((a, b), c) -> (8 * (a |> Vector24.indexToInt) + ((b |> Vector8.indexToInt)), c |> Vector24.indexToInt))
+      in
       describe "calc indexes"
          [
          test "line 0" <|
             \_ ->
                let
-                  data = screenOffsets |> List.take 8
+                  data = numericOffsets |> List.take 8
                in
                   Expect.equal [(0,0),(8,1),(16,2),(24,3),(32,4),(40,5),(48,6),(56,7)] data
          ,test "line 8" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 8 |> List.take 8
+                  data = numericOffsets |> List.drop 8 |> List.take 8
                in
                   Expect.equal [(1,0),(9,1),(17,2),(25,3),(33,4),(41,5),(49,6),(57,7)] data
          ,test "line 16" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 16 |> List.take 8
+                  data = numericOffsets |> List.drop 16 |> List.take 8
                in
                   Expect.equal [(2,0),(10,1),(18,2),(26,3),(34,4),(42,5),(50,6),(58,7)] data
          ,test "line 24" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 24 |> List.take 8
+                  data = numericOffsets |> List.drop 24 |> List.take 8
                in
                   Expect.equal [(3,0),(11,1),(19,2),(27,3),(35,4),(43,5),(51,6),(59,7)] data
          ,test "line 32" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 32 |> List.take 8
+                  data = numericOffsets |> List.drop 32 |> List.take 8
                in
                   Expect.equal [(4,0),(12,1),(20,2),(28,3),(36,4),(44,5),(52,6),(60,7)] data
          ,test "line 40" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 40 |> List.take 8
+                  data = numericOffsets |> List.drop 40 |> List.take 8
                in
                   Expect.equal [(5,0),(13,1),(21,2),(29,3),(37,4),(45,5),(53,6),(61,7)] data
          ,test "line 48" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 48 |> List.take 8
+                  data = numericOffsets |> List.drop 48 |> List.take 8
                in
                   Expect.equal [(6,0),(14,1),(22,2),(30,3),(38,4),(46,5),(54,6),(62,7)] data
          ,test "line 56" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 56 |> List.take 8
+                  data = numericOffsets |> List.drop 56 |> List.take 8
                in
                   Expect.equal [(7,0),(15,1),(23,2),(31,3),(39,4),(47,5),(55,6),(63,7)] data
          ,test "line 64" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 64 |> List.take 8
+                  data = numericOffsets |> List.drop 64 |> List.take 8
                in
                   Expect.equal [(64,8),(72,9),(80,10),(88,11),(96,12),(104,13),(112,14),(120,15)] data
          ,test "line 128" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 128 |> List.take 8
+                  data = numericOffsets |> List.drop 128 |> List.take 8
                in
                   Expect.equal [(128,16),(136,17),(144,18),(152,19),(160,20),(168,21),(176,22),(184,23)] data
          ,test "line 191" <|
             \_ ->
                let
-                  data = screenOffsets |> List.drop 184 |> List.take 8
+                  data = numericOffsets |> List.drop 184 |> List.take 8
                in
                   Expect.equal [(135,16),(143,17),(151,18),(159,19),(167,20),(175,21),(183,22),(191,23)] data
          ]
