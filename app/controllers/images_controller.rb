55aee332fc01ab70d78560bc8bbb837103b25631
starswan <github@starswan.com>
2024-10-01 18:47:48 +0100

    reduce scope

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,11 +1,13 @@
 class ImagesController < ApplicationController
   def show
-    # data is 0..65535
+    # data is 0..65535 - so 8x2 pixels (4 needed for a screen square)
+    # could start with 0..255 for simplicity (maybe hex string?)
     @data = params[:data]
+    # we can do fg / bg using CSS
     # foreground is 0..7
-    @foreground = params[:foreground]
+    # @foreground = params[:foreground]
     # background is 0..7
-    @background = params[:background]
+    # @background = params[:background]
     @image = MiniMagick::Image.new do |image|
       image.draw_point 0, 0
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :games, only: [:index, :show]
   # resources :images, only: :get
-  get 'images/:data/:foreground/:background' => 'images#show', :as=>:image
+  get 'images/:data' => 'images#show', :as=>:image
 end
