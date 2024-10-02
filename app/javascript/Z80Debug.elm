d01e95c1d85b255980b246d4a7a5bfa2c5e00f0d
starswan <github@starswan.com>
2024-10-02 16:15:22 +0100

    undebug module

--- app/javascript/Z80Debug.elm
+++ app/javascript/Z80Debug.elm
@@ -1,21 +1,9 @@
 module Z80Debug exposing (..)
 
 
-debugEnabled =
-    False
-
-
 debugLog msg thingToLog thingToReturn =
-    if debugEnabled then
         Debug.log msg thingToLog |> (\_ -> thingToReturn)
-
-    else
-        thingToReturn
 
 
 debugTodo msg thingToLog thingToReturn =
-    if debugEnabled then
         Debug.todo (msg ++ " " ++ thingToLog) |> (\_ -> thingToReturn)
-
-    else
-        thingToReturn
