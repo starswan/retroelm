de64b04ef1d8df8439329bf80bfe27e3b0e36362
Stephen Dicks <github@starswan.com>
2024-10-05 14:31:14 +0100

    debug for tap loading (#280)

--- app/javascript/Tapfile.elm
+++ app/javascript/Tapfile.elm
@@ -2,13 +2,15 @@ module Tapfile exposing (..)
 
 import Bytes exposing (Bytes, Endianness(..), width)
 import Bytes.Decode exposing (Decoder, Step(..), andThen, fail, loop, map, map2, map3, map4, map5, string, succeed, unsignedInt16, unsignedInt8)
+import Utils exposing (toPlainHexString2)
 import Z80Debug exposing (debugLog)
+
 
 parseTapFile : Bytes -> List Tapfile
 parseTapFile bytes =
     let
         result =
-            Bytes.Decode.decode (tapfile_list_decoder (bytes |> width) tapfile_decoder) bytes
+            Bytes.Decode.decode (tapfileListDecoder (bytes |> width) tapfileDecoder) bytes
     in
     case result of
         Just list ->
@@ -18,32 +20,44 @@ parseTapFile bytes =
             []
 
 
-type HeaderType
-    = PROGRAM
-    | NUMBER_ARRAY
-    | CHAR_ARRAY
-    | CODE
-
-
-type alias TapeHeaderStart =
-    { headerLength : Int
-    , headerFlagByte : Int
-    , headerType : HeaderType
-    }
-
-
 type alias TapeHeaderEnd =
     { filename : String
-    , block_length : Int
+    , blockLength : Int
     , parameter_1 : Int
     , parameter_2 : Int
     , checksum : Int
     }
 
 
+type alias ProgramTapeHeader =
+    { filename : String
+    , blockLength : Int
+    , autoStartLineNumber : Int
+    , startOfVariablesArea : Int
+    , checksum : Int
+    }
+
+
+type alias CodeTapeHeader =
+    { filename : String
+    , blockLength : Int
+    , startOfCodeBlock : Int
+    , threeTwoSevenSixEight : Int
+    , checksum : Int
+    }
+
+
+type TapfileData
+    = Program ProgramTapeHeader
+    | NumberArray TapeHeaderEnd
+    | CharArray TapeHeaderEnd
+    | Code CodeTapeHeader
+
+
 type alias TapfileHeader =
-    { start : TapeHeaderStart
-    , end : TapeHeaderEnd
+    { length : Int
+    , flagByte : Int
+    , data : TapfileData
     }
 
 
@@ -56,7 +70,9 @@ type alias TapfileBlock =
 
 
 type alias Tapfile =
-    { header : TapfileHeader
+    { length : Int
+    , flagByte : Int
+    , data : TapfileData
     , block : TapfileBlock
     }
 
@@ -65,96 +81,165 @@ spectrumUnsigned16Bit =
     unsignedInt16 LE
 
 
-tapfile_list_decoder : Int -> Decoder Tapfile -> Decoder (List Tapfile)
-tapfile_list_decoder len decoder =
+tapfileListDecoder : Int -> Decoder Tapfile -> Decoder (List Tapfile)
+tapfileListDecoder len decoder =
     let
-        y = debugLog "TAP file size" len Nothing
+        y =
+            debugLog "TAP file size" len Nothing
     in
-    loop ( len, [] ) (tapfile_step_decoder decoder)
+    loop ( len, [] ) (tapfileStepDecoder decoder)
 
 
-tapfile_step_decoder : Decoder Tapfile -> ( Int, List Tapfile ) -> Decoder (Step ( Int, List Tapfile ) (List Tapfile))
-tapfile_step_decoder decoder ( n, xs ) =
+tapfileStepDecoder : Decoder Tapfile -> ( Int, List Tapfile ) -> Decoder (Step ( Int, List Tapfile ) (List Tapfile))
+tapfileStepDecoder decoder ( n, xs ) =
     if n <= 0 then
         succeed (Done xs)
 
     else
-        map (\x -> Loop ( n - x.header.start.headerLength - 2 - (x.block.data |> List.length) - 4, x :: xs )) decoder
+        map (\x -> Loop ( n - x.length - 2 - (x.block.data |> List.length) - 4, x :: xs )) decoder
 
 
-list_with_length_decoder : Int -> Decoder a -> Decoder (List a)
-list_with_length_decoder len decoder =
+listWithLengthDecoder : Int -> Decoder Int -> Decoder (List Int)
+listWithLengthDecoder len decoder =
     loop ( len, [] ) (listStep decoder)
 
 
-listStep : Decoder a -> ( Int, List a ) -> Decoder (Step ( Int, List a ) (List a))
+listStep : Decoder Int -> ( Int, List Int ) -> Decoder (Step ( Int, List Int ) (List Int))
 listStep decoder ( n, xs ) =
     if n <= 0 then
-        succeed (Done xs)
-
+        succeed (Done (xs |> List.reverse))
     else
         map (\x -> Loop ( n - 1, x :: xs )) decoder
 
 
-tapfile_decoder : Decoder Tapfile
-tapfile_decoder =
-    tapeHeader |> andThen decodeTapBody
+tapFilename : TapfileData -> String
+tapFilename tapFileHeader =
+    case tapFileHeader of
+        Program end ->
+            end.filename
+
+        NumberArray end ->
+            end.filename
+
+        CharArray end ->
+            end.filename
+
+        Code end ->
+            end.filename
+
+
+tapBlockLength : TapfileData -> Int
+tapBlockLength tapFileHeader =
+    case tapFileHeader of
+        Program end ->
+            end.blockLength
+
+        NumberArray end ->
+            end.blockLength
+
+        CharArray end ->
+            end.blockLength
+
+        Code end ->
+            end.blockLength
+
+
+tapeheaderEndDecoder : Decoder TapeHeaderEnd
+tapeheaderEndDecoder =
+    map5 TapeHeaderEnd (string 10) spectrumUnsigned16Bit spectrumUnsigned16Bit spectrumUnsigned16Bit unsignedInt8
+
+
+programHeaderDecoder : Decoder ProgramTapeHeader
+programHeaderDecoder =
+    map5 ProgramTapeHeader (string 10) spectrumUnsigned16Bit spectrumUnsigned16Bit spectrumUnsigned16Bit unsignedInt8
+
+
+codeHeaderDecoder : Decoder CodeTapeHeader
+codeHeaderDecoder =
+    map5 CodeTapeHeader (string 10) spectrumUnsigned16Bit spectrumUnsigned16Bit spectrumUnsigned16Bit unsignedInt8
+
+
+tapfileDataDecoder : Int -> Decoder TapfileData
+tapfileDataDecoder headerType =
+    case headerType of
+        0 ->
+            map Program programHeaderDecoder
+
+        1 ->
+            map NumberArray tapeheaderEndDecoder
+
+        2 ->
+            map CharArray tapeheaderEndDecoder
+
+        3 ->
+            map Code codeHeaderDecoder
+
+        _ ->
+            fail
 
 
 decodeTapBody : TapfileHeader -> Decoder Tapfile
 decodeTapBody tapfileheader =
     let
         x =
-            debugLog "filename blocklen" ( tapfileheader.end.filename, tapfileheader.end.block_length ) Nothing
+            debugLog "filename blocklen" ( tapfileheader.data |> tapFilename, tapfileheader.data |> tapBlockLength ) Nothing
 
         block_decoder =
-            tapFileBlock tapfileheader.end.block_length
+            tapFileBlockDecoder (tapfileheader.data |> tapBlockLength)
     in
-    block_decoder |> andThen (grabWholeThing tapfileheader)
+    block_decoder |> andThen (grabWholeThingDecoder tapfileheader)
 
 
-grabWholeThing : TapfileHeader -> TapfileBlock -> Decoder Tapfile
-grabWholeThing tapfileheader tapfile_body =
-    succeed (Tapfile tapfileheader tapfile_body)
+-- programs are <line num> <length> <data of length length>
+-- which might be better done using a decoder?
+debugProgram : List Int -> String
+debugProgram list =
+    list
+        |> List.map toPlainHexString2
+        |> List.foldl
+            (\i s ->
+                if (s |> String.length) == 0 then
+                    i
+
+                else
+                    s ++ " " ++ i
+            )
+            ""
 
 
-headerTypeFromInt : Int -> Decoder HeaderType
-headerTypeFromInt headerType =
+grabWholeThingDecoder : TapfileHeader -> TapfileBlock -> Decoder Tapfile
+grabWholeThingDecoder tapfileheader tapfile_body =
     let
-        header_int = debugLog "Header Type" headerType headerType
+        x = case tapfileheader.data of
+            Program _ ->
+                debugLog "Program " (tapfile_body.data |> debugProgram) Nothing
+            NumberArray _ -> Nothing
+            CharArray _ -> Nothing
+            Code _ -> Nothing
     in
-    case header_int of
-        0 ->
-            succeed PROGRAM
-
-        1 ->
-            succeed NUMBER_ARRAY
-
-        2 ->
-            succeed CHAR_ARRAY
-
-        3 ->
-            succeed CODE
-
-        _ ->
-            fail
+    succeed (Tapfile tapfileheader.length tapfileheader.flagByte tapfileheader.data tapfile_body)
 
 
-tapeheaderStart : Decoder TapeHeaderStart
-tapeheaderStart =
-    map3 TapeHeaderStart spectrumUnsigned16Bit unsignedInt8 (unsignedInt8 |> andThen headerTypeFromInt)
+tapfileDecoder : Decoder Tapfile
+tapfileDecoder =
+    decodeTapeHeader |> andThen decodeTapBody
 
 
-tapeheaderEnd : Decoder TapeHeaderEnd
-tapeheaderEnd =
-    map5 TapeHeaderEnd (string 10) spectrumUnsigned16Bit spectrumUnsigned16Bit spectrumUnsigned16Bit unsignedInt8
+decodeTapeHeader : Decoder TapfileHeader
+decodeTapeHeader =
+    let
+        length =
+            spectrumUnsigned16Bit
+
+        flagByte =
+            unsignedInt8
+
+        headerType =
+            unsignedInt8
+    in
+    map3 TapfileHeader length flagByte (headerType |> andThen tapfileDataDecoder)
 
 
-tapeHeader : Decoder TapfileHeader
-tapeHeader =
-    map2 TapfileHeader tapeheaderStart tapeheaderEnd
-
-
-tapFileBlock : Int -> Decoder TapfileBlock
-tapFileBlock block_length =
-    map4 TapfileBlock spectrumUnsigned16Bit unsignedInt8 (list_with_length_decoder block_length unsignedInt8) unsignedInt8
+tapFileBlockDecoder : Int -> Decoder TapfileBlock
+tapFileBlockDecoder blockLength =
+    map4 TapfileBlock spectrumUnsigned16Bit unsignedInt8 (listWithLengthDecoder blockLength unsignedInt8) unsignedInt8
