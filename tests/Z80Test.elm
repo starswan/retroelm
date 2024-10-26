60d5381812ec2311fc63b5c669cfc9188722e758
starswan <github@starswan.com>
2024-10-26 10:59:46 +0100

    gerouop D0 test

--- tests/Z80Test.elm
+++ tests/Z80Test.elm
@@ -245,35 +245,9 @@ suite =
                   Expect.equal { pc = (addr + 1), fa = 6, fb = -7, ff = 0, fr = 0 }
                   { pc = new_z80.pc, fa = new_z80.flags.fa, fb =  new_z80.flags.fb, ff = new_z80.flags.ff, fr = new_z80.flags.fr }
          ],
-         describe "EXX (0xD9)"
-         [
-            test "doit" <|
-            \_ ->
-               let
-                  alt = z80.alt_main
-                  new_env = z80env
-                               |> setMem addr 0xD9
-                               |> setMem (addr + 1) 0x16
-                  new_z80 = execute_instruction z80rom { z80 | env = { new_env | sp = 0xFF77 },
-                                                        alt_main = { alt | hl = 0x4040, b = 0x67, c = 0x34, d = 0x12, e = 0x81 },
-                                                        main = { z80main | hl = 0x5050, d = 0x60, e = 0x00, b = 0x00, c = 0x05 } }
-               in
-                  Expect.equal {pc=(addr + 1), hl=0x4040}  {pc=new_z80.pc, hl=new_z80.main.hl}
-         ],
          describe "16 bit Pop"
          [
-            test "POP DE (0xD1)" <|
-            \_ ->
-               let
-                  new_env = z80env
-                               |> setMem addr 0xD1
-                               |> setMem 0xFF77 0x16
-                               |> setMem 0xFF78 0x56
-                  new_z80 = execute_instruction z80rom { z80 | env = { new_env | sp = 0xFF77 },
-                                                        main = { z80main | hl = 0x5050, d = 0x60, e = 0x00, b = 0x00, c = 0x05 } }
-               in
-                  Expect.equal {pc=(addr + 1), d=0x56, e=0x16, sp=0xFF79}  {sp=new_z80.env.sp, pc=new_z80.pc, d=new_z80.main.d, e=new_z80.main.e}
-            ,test "POP HL (0xE1)" <|
+            test "POP HL (0xE1)" <|
             \_ ->
                let
                   new_env = z80env
