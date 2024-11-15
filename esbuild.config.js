63ba5e1192643a052dc32c36394ea80e9e7be811
starswan <github@starswan.com>
2024-11-15 19:52:45 +0000

    esbuild watch with <0.16

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,9 +1,13 @@
 // esbuild.config.js
 const path = require('path')
+
+// This is a CommonJS module, so cannot be imported as an ES module
 const ElmPlugin = require('esbuild-plugin-elm')
 const esbuild = require('esbuild')
 
 const node_env = process.env.RAILS_ENV
+const watch = process.argv.includes("--watch");
+
 
 const elm_options = function() {
     // can't see this log message?
@@ -23,15 +27,21 @@ const elm_options = function() {
 
 const options = elm_options()
 
-// the absWorkingDirectory set below allows us to use paths relative to that location
-esbuild.build({
+// watch option only works with esbuild < 0.16
+// https://stackoverflow.com/questions/75221520/invalid-option-in-build-call-watch
+
+const config = {
     entryPoints: ['./hello_elm.js'],
     bundle: true,
     outdir: path.join(process.cwd(), "app/assets/builds"),
     absWorkingDir: path.join(process.cwd(), "app/javascript"),
-    // watch: process.argv.includes("--watch"),
+    watch: watch,
     sourcemap: true,
     plugins: [
         ElmPlugin(options) // options are documented below
     ],
-}).catch(e => (console.error(e), process.exit(1)))
+}
+esbuild.build(config);
+// esbuild.context(config).then((r) => {
+//     r.watch();
+// });
