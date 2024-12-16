starswan/qaop-elm

link games and z80test

config/deploy.rb
@@ -28,6 +28,7 @@ end
 
 # runtime dependencies
 # depend :remote, :gem, "bundler", ">=1.0.0.rc.2"
+set :linked_dirs, fetch(:linked_dirs, []) << "games" << "z80test"
 
 # tasks
 namespace :deploy do

starswan/retroelm

config/deploy.rb
@@ -28,6 +28,7 @@ end
 
 # runtime dependencies
 # depend :remote, :gem, "bundler", ">=1.0.0.rc.2"
+set :linked_files, fetch(:linked_files, []) << "games" << "z80test"
 
 # tasks
 namespace :deploy do